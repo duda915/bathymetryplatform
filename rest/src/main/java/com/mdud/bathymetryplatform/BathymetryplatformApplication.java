cbfb8698bc651bd496581489e5c5d2fb7c4e15d6
MDud <duda915@gmail.com>
2018-11-07 22:27:29 +0100

    Spring init

---
+++ rest/src/main/java/com/mdud/bathymetryplatform/BathymetryplatformApplication.java
@@ -0,0 +1,12 @@
+package com.mdud.bathymetryplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BathymetryplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BathymetryplatformApplication.class, args);
+	}
+}
---
+++ rest/src/test/java/com/mdud/bathymetryplatform/BathymetryplatformApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mdud.bathymetryplatform;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BathymetryplatformApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
