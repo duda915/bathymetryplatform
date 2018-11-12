7986ea6432eed9572303f3c3f2900ea2c64487d9
MDud <duda915@gmail.com>
2018-11-12 17:14:20 +0100

    APP login form

--- app/bathymetry-app/src/components/LoginControl.js
+++ app/bathymetry-app/src/components/LoginControl.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import Alert from 'react-bootstrap/lib/Alert';
-import Container from 'react-bootstrap/lib/Container';
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
+import Form from 'react-bootstrap/lib/Form';
+import Button from 'react-bootstrap/lib/Button';
 
 class LoginControl extends Component {
 
@@ -14,7 +14,24 @@ class LoginControl extends Component {
                 <Row>
                     <Col xs={4}/>
                     <Col xs={4}>
-                        
+                        <Form>
+                            <Form.Group controlId="formUsername">
+                                <Form.Label>Username</Form.Label>
+                                <Form.Control type="text" placeholder="Enter username"/>
+                            </Form.Group>
+                            
+                            <Form.Group controlId="formPassword">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control type="password" placeholder="Enter password"/>
+                            </Form.Group>
+
+                            <Form.Group controlId="formCheckbox">
+                                <Form.Check type="checkbox" label="Register"/>
+                            </Form.Group>
+                            <Button variant="primary" type="submit">
+                                Login
+                            </Button>
+                        </Form>
                     </Col>
                     <Col xs={4}/>
                 </Row>
