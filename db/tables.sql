93fc7a661f69c41b6cb756ed9a127558126bf458
MDud <duda915@gmail.com>
2018-11-14 00:57:47 +0100

    DB tables bracket typo

--- db/tables.sql
+++ db/tables.sql
@@ -6,7 +6,7 @@ DROP TABLE IF EXISTS bathymetry CASCADE;
 CREATE TABLE bathymetry (gid SERIAL PRIMARY KEY, meta_id INTEGER REFERENCES bathymetry_meta(id), coords GEOMETRY(POINT, 4326), measure DECIMAL);
 
 INSERT INTO bathymetry_meta (name, collection_date, author) VALUES ('testname', '2000-10-10', 'testauthor');
-INSERT INTO bathymetry (meta_id, coords, measure) VALUES (1, 'SRID=4326;POINT(-109 29)', 15.2));
+INSERT INTO bathymetry (meta_id, coords, measure) VALUES (1, 'SRID=4326;POINT(-109 29)', 15.2);
 -- geoserver
 
 
