1e0c5819a525e1e8c027cf79380d2e9a2d818106
MDud <duda915@gmail.com>
2018-11-21 19:47:03 +0100

    APP login component rewritten to primereact

--- app/bathymetry-app/src/components/LoginControl.js
+++ app/bathymetry-app/src/components/LoginControl.js
@@ -2,8 +2,18 @@ import React, { Component } from 'react';
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
 import Form from 'react-bootstrap/lib/Form';
-import Button from 'react-bootstrap/lib/Button';
+// import Button from 'react-bootstrap/lib/Button';
 
+
+import {Panel} from 'primereact/panel';
+import {InputText} from 'primereact/inputtext';
+import {Password} from 'primereact/password';
+import {Button} from 'primereact/button';
+
+import 'primereact/resources/primereact.min.css';
+import 'primereact/resources/themes/nova-colored/theme.css';
+import 'primeflex/primeflex.css';
+import 'primeicons/primeicons.css';
 import {RestFetch} from './utility/Rest';
 
 
@@ -48,33 +58,78 @@ class LoginControl extends Component {
 
     render() {
         return(
-            <div className="container-fluid">
-                <Row style={{height: '100px'}}>
-                </Row>
-                <Row>
-                    <Col xs={4}/>
-                    <Col xs={4} className="border border-primary p-4">
-                        <Form onSubmit={this.handleSubmit}>
-                            <Form.Group controlId="formUsername">
-                                <Form.Label>Username</Form.Label>
-                                <Form.Control required type="text" placeholder="Enter username" name="username" value={this.state.username} onChange={this.handleChange}/>
-                            </Form.Group>
+            // <div className="container-fluid">
+            //     <Row style={{height: '100px'}}>
+            //     </Row>
+            //     <Row>
+            //         <Col xs={4}/>
+            //         <Col xs={4} className="border border-primary p-4">
+            //             <Form onSubmit={this.handleSubmit}>
+            //                 <Form.Group controlId="formUsername">
+            //                     <Form.Label>Username</Form.Label>
+            //                     <Form.Control required type="text" placeholder="Enter username" name="username" value={this.state.username} onChange={this.handleChange}/>
+            //                 </Form.Group>
                             
-                            <Form.Group controlId="formPassword">
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control required type="password" placeholder="Enter password" name="password" value={this.state.password} onChange={this.handleChange}/>
-                            </Form.Group>
+            //                 <Form.Group controlId="formPassword">
+            //                     <Form.Label>Password</Form.Label>
+            //                     <Form.Control required type="password" placeholder="Enter password" name="password" value={this.state.password} onChange={this.handleChange}/>
+            //                 </Form.Group>
 
-                            <Form.Group controlId="formCheckbox">
-                                <Form.Check type="checkbox" label="Register" name="register" checked={this.state.register} onChange={this.handleChange}/>
-                            </Form.Group>
-                            <Button variant="primary" type="submit" >
-                                Login
-                            </Button>
-                        </Form>
-                    </Col>
-                    <Col xs={4}/>
-                </Row>
+            //                 <Form.Group controlId="formCheckbox">
+            //                     <Form.Check type="checkbox" label="Register" name="register" checked={this.state.register} onChange={this.handleChange}/>
+            //                 </Form.Group>
+            //                 <Button variant="primary" type="submit" >
+            //                     Login
+            //                 </Button>
+            //             </Form>
+            //         </Col>
+            //         <Col xs={4}/>
+            //     </Row>
+            // </div>
+            <div className="loginControl">
+                <div className="p-grid p-fluid" >
+                
+                    {/* first row */}
+                    <div className="p-col-12" style={{height: '25vh'}}></div>
+
+                    {/* login row */}
+                    <div className="p-col-1 p-lg-4"></div>
+                    <div className="p-col p-lg-4">
+                        <Panel header="Login">
+                            <form onSubmit={this.handleSubmit}>
+                                <div className="p-grid">
+                                    <div className="p-col">
+                                        <div className="p-inputgroup">
+                                            <span className="p-inputgroup-addon">
+                                                <i className="pi pi-user"></i>
+                                            </span>
+                                            <InputText placeholder="Username" name="username" value={this.state.username} onChange={this.handleChange}></InputText>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="p-grid">
+                                    <div className="p-col">
+                                        <div className="p-inputgroup">
+                                            <span className="p-inputgroup-addon">
+                                                <i className="pi pi-key"></i>
+                                            </span>
+                                            <Password placeholder="Enter password" feedback={false} name="password" value={this.state.password} onChange={this.handleChange}></Password>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="p-grid p-fluid">
+                                    <div className="p-col p-md-4"></div>
+                                    <div className="p-col p-md-4"></div>
+                                    <div className="p-col p-md-4">
+                                        <Button label="Login" type="submit"/>
+                                    </div>
+                                </div>
+                            </form>
+                        </Panel>
+                    </div>
+                    <div className="p-col-1 p-lg-4"></div>
+
+                </div>
             </div>
         );
     }
