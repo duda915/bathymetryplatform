bc6ed94e6c624d4d81b76a434a5c6904c56a4226
MDud <duda915@gmail.com>
2018-11-26 00:49:55 +0100

    Start tutorial, docker-compose based starting

---
+++ docker/first/postgiscontainer/initdb.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -x
+PASSWORD=docker
+USER=docker;
+
+PGPASSWORD=$PASSWORD PGUSER=$USER createdb -h localhost bathymetry
+PGPASSWORD=$PASSWORD PGUSER=$USER psql -h localhost -d bathymetry -c 'CREATE EXTENSION postgis'
+PGPASSWORD=$PASSWORD PGUSER=$USER psql -h localhost -d bathymetry < tables.sql
+PGPASSWORD=$PASSWORD PGUSER=$USER createdb -h localhost epsg
+PGPASSWORD=$PASSWORD PGUSER=$USER psql -h localhost -d epsg < PostgreSQL_Table_Script.sql
+PGPASSWORD=$PASSWORD PGUSER=$USER psql -h localhost -d epsg < PostgreSQL_Data_Script.sql
+PGPASSWORD=$PASSWORD PGUSER=$USER psql -h localhost -d epsg < PostgreSQL_FKey_Script.sql
