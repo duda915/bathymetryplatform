9308466932d21997558be1e24e36a5707b3b7c42
MDud <duda915@gmail.com>
2019-02-02 13:37:27 +0100

    REST deleted oauth tables

--- rest/src/main/resources/schema.sql
+++ rest/src/main/resources/schema.sql
@@ -30,21 +30,3 @@ CREATE TABLE IF NOT EXISTS bathymetry_point (
   depth DECIMAL
 );
 
--- oauth tables
-DROP TABLE IF EXISTS oauth_access_token;
-CREATE TABLE oauth_access_token (
-  token_id VARCHAR(256),
-  token BYTEA,
-  authentication_id VARCHAR(256) PRIMARY KEY,
-  user_name VARCHAR(256),
-  client_id VARCHAR(256),
-  authentication BYTEA,
-  refresh_token VARCHAR(256)
-);
-
-DROP TABLE IF EXISTS oauth_refresh_token;
-CREATE TABLE oauth_refresh_token (
-  token_id VARCHAR(256),
-  token BYTEA,
-  authentication BYTEA
-);
