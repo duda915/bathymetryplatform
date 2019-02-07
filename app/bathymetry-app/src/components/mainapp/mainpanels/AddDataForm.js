96d05ac5c6eb2d85850a29e49b3f98afbbe8ed8c
MDud <duda915@gmail.com>
2019-02-07 21:52:45 +0100

    APP form for adding data refactored to separate component

---
+++ app/bathymetry-app/src/components/mainapp/mainpanels/AddDataForm.js
@@ -0,0 +1,169 @@
+import React, { Component } from 'react';
+import { Panel } from 'primereact/panel';
+import { InputText } from 'primereact/inputtext';
+import { Calendar } from 'primereact/calendar';
+import { FileUpload } from 'primereact/fileupload';
+import { Button } from 'primereact/button';
+import DataService from '../../../services/DataService';
+import UserService from '../../../services/UserService';
+import BathymetryDataSetDTO from '../../../services/dtos/BathymetryDataSetDTO';
+import EPSGCodeService from '../../../services/EPSGCodeService';
+
+export class AddDataForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dataName: '',
+            date: '',
+            dataOwner: '',
+            crs: '',
+            file: null,
+
+            epsgSuggestions: null
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.onFileSelect = this.onFileSelect.bind(this);
+        this.onSelectAbort = this.onSelectAbort.bind(this);
+
+        this.dataService = new DataService();
+        this.userService = new UserService();
+        this.epsgService = new EPSGCodeService();
+
+
+        this.epsgCodes = [];
+    }
+
+    componentDidMount() {
+        this.fetchEpsgCodes();
+        this.getUsername();
+        this.getTodayDate();
+    }
+
+    fetchEpsgCodes() {
+        this.epsgService.getEPSGCodes()
+            .then(response => {
+                this.epsgCodes = response.data;
+                console.log(this.epsgCodes);
+            })
+    }
+
+    getTodayDate() {
+        let date = new Date();
+
+        this.setState({
+            date: date
+        })
+    }
+
+    getUsername() {
+        this.userService.getUser()
+            .then(response => {
+                this.setState({
+                    dataOwner: response.data.username,
+                });
+            })
+    }
+
+    handleChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    onFileSelect(event) {
+        console.log(event.files[0]);
+        this.setState({
+            file: event.files[0],
+            dataName: event.files[0].name,
+        })
+    }
+
+    onSelectAbort(event) {
+        //prevent default primereact behaviour
+        event.xhr.open('GET', '', true);
+        this.setState({
+            file: null,
+        })
+    }
+
+    handleSubmit(event) {
+        const dataSetDTO = new BathymetryDataSetDTO(this.state.crs, this.state.dataName,
+            new Date(), this.state.dataOwner);
+
+        this.props.loadingService(true);
+
+        this.dataService.addData(dataSetDTO, this.state.file)
+            .then(response => {
+                this.props.messageService('success', "Success", "file upload success");
+            })
+            .catch(error => {
+                this.props.messageService('error', 'Error', error.response.data.message);
+            })
+            .finally(response => {
+                this.props.loadingService(false);
+                this.props.fetchUserDataSets();
+            });
+
+        event.preventDefault();
+    }
+
+    suggestEpsgCode(event) {
+        let results = this.epsgCodes.filter((code) => {
+            return code.epsgCode.toLowerCase().startsWith(event.query.toLowerCase());
+        })
+
+        this.setState({ epsgSuggestions: results });
+    }
+
+    render() {
+        return <Panel header="Add Data">
+            <form onSubmit={this.handleSubmit} autoComplete="off">
+                <div className="p-grid">
+                    <div className="p-col-12">
+                        <FileUpload mode="basic" accept="*" maxFileSize={50000000} onSelect={this.onFileSelect} onBeforeSend={this.onSelectAbort} />
+                    </div>
+                    <div className="p-col-12">
+                        <div className="p-inputgroup">
+                            <span className="p-inputgroup-addon">
+                                <i className="pi pi-tag"></i>
+                            </span>
+                            <InputText placeholder="Data name" name="dataName" value={this.state.dataName} onChange={this.handleChange}></InputText>
+                        </div>
+                    </div>
+                    <div className="p-col-12">
+                        <div className="p-inputgroup">
+                            <span className="p-inputgroup-addon">
+                                <i className="pi pi-user"></i>
+                            </span>
+                            <InputText placeholder="Data owner" name="dataOwner" value={this.state.dataOwner} onChange={this.handleChange}></InputText>
+                        </div>
+                    </div>
+                    <div className="p-col-12">
+                        <Calendar dateFormat="dd/mm/yy" value={this.state.date} onChange={e => this.setState({
+                            date: e.value
+                        })} showIcon={true}></Calendar>
+                    </div>
+                    <div className="p-col-12">
+                        <div className="p-inputgroup">
+                            <span className="p-inputgroup-addon">
+                                <i className="pi pi-globe"></i>
+                            </span>
+                            <InputText placeholder="EPSG" name="crs" value={this.state.crs} onChange={this.handleChange}></InputText>
+                        </div>
+                    </div>
+
+                    <div className="p-col-12">
+                        <Button label="Upload" type="submit" />
+                    </div>
+                </div>
+            </form>
+        </Panel>;
+    }
+
+}
--- app/bathymetry-app/src/components/mainapp/mainpanels/DataManager.js
+++ app/bathymetry-app/src/components/mainapp/mainpanels/DataManager.js
@@ -1,63 +1,33 @@
+import { AddDataForm } from './AddDataForm';
 import React, { Component } from 'react';
-import { Panel } from 'primereact/panel';
-import { InputText } from 'primereact/inputtext';
-import { Calendar } from 'primereact/calendar';
-import { FileUpload } from 'primereact/fileupload';
-import { Button } from 'primereact/button';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
-import {Growl} from 'primereact/growl';
-import {ContextMenu} from 'primereact/contextmenu';
+import { ContextMenu } from 'primereact/contextmenu';
 import DataService from '../../../services/DataService';
-import LoadingComponent from '../../utility/LoadingComponent';
 import UserService from '../../../services/UserService';
-import BathymetryDataSetDTO from '../../../services/dtos/BathymetryDataSetDTO';
 
 export default class DataManager extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            dataName: '',
-            date: '',
-            dataOwner: '',
-            crs: '',
-            file: null,
             contextMenu: [
-                {label: 'Delete', icon: 'pi pi-fw pi-times', command: (event) => this.deleteDataSet(this.state.selectedData)}
-            ]
-        }
+                {
+                    label: 'Delete', icon: 'pi pi-fw pi-times',
+                    command: (event) => this.deleteDataSet(this.state.selectedData)
+                }
+            ],
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.onFileSelect = this.onFileSelect.bind(this);
-        this.onSelectAbort = this.onSelectAbort.bind(this);
+        }
 
         this.dataService = new DataService();
         this.userService = new UserService();
+
+        this.fetchUserDataSets = this.fetchUserDataSets.bind(this)
     }
 
     componentDidMount() {
         this.fetchUserDataSets();
-        this.getUsername();
-        this.getTodayDate();
-    }
-
-    getTodayDate() {
-        let date = new Date();
-
-        this.setState({
-            date: date
-        })
-    }
-
-    getUsername() {
-        this.userService.getUser()
-        .then(response => {
-            this.setState({
-                dataOwner: response.data.username,
-            });
-        })
     }
 
     fetchUserDataSets() {
@@ -65,121 +35,32 @@ export default class DataManager extends Component {
             .then(response => this.setState({ data: response.data }));
     }
 
-    handleChange(event) {
-        const target = event.target;
-        const value = target.type === 'checkbox' ? target.checked : target.value;
-        const name = target.name;
-
-        this.setState({
-            [name]: value
-        });
-    }
-
-    onFileSelect(event) {
-        console.log(event.files[0]);
-        this.setState({
-            file: event.files[0],
-            dataName: event.files[0].name,
-        })
-    }
-
-    onSelectAbort(event) {
-        //prevent default primereact behaviour
-        event.xhr.open('GET', '', true);
-        this.setState({
-            file: null,
-        })
-    }
-
-    handleSubmit(event) {
-        const dataSetDTO = new BathymetryDataSetDTO(this.state.crs, this.state.dataName,
-            new Date(), this.state.dataOwner);
-
+    deleteDataSet(dataSet) {
         this.props.loadingService(true);
-
-        this.dataService.addData(dataSetDTO, this.state.file)
+        this.dataService.deleteData(dataSet.id)
             .then(response => {
-                this.props.messageService('success', "Success", "file upload success");
+                this.props.messageService('info', 'Success', 'date deleted');
             })
-            .catch(error => {
-                this.props.messageService('error', 'Error', error.response.data.message);
+            .catch(response => {
+                this.props.messageService('error', 'Error', 'cannot delete data');
             })
             .finally(response => {
                 this.props.loadingService(false);
                 this.fetchUserDataSets();
-            });
-
-        event.preventDefault();
+            })
     }
-
-    deleteDataSet(dataSet) {
-        this.props.loadingService(true);
-        this.dataService.deleteData(dataSet.id)  
-        .then(response => {
-            this.props.messageService('info', 'Success', 'date deleted');
-        })
-        .catch(response => {
-            this.props.messageService('error', 'Error', 'cannot delete data');
-        })
-        .finally(response => {
-            this.props.loadingService(false);
-            this.fetchUserDataSets();
-        })
-    }
-
 
     render() {
         return (
             <div className="bathymetry-app-padding">
-                <Growl ref={(ref) => this.growl = ref} />
-                <LoadingComponent ref={(ref) => this.progress = ref}/>
                 <div className="p-grid p-fluid ">
                     <div className="p-col-12 p-lg-3">
-                        <Panel header="Add Data">
-                            <form onSubmit={this.handleSubmit} autoComplete="off">
-                                <div className="p-grid">
-                                    <div className="p-col-12">
-                                        <FileUpload mode="basic" accept="*" maxFileSize={50000000} onSelect={this.onFileSelect} onBeforeSend={this.onSelectAbort} />
-                                    </div>
-                                    <div className="p-col-12">
-                                        <div className="p-inputgroup">
-                                            <span className="p-inputgroup-addon">
-                                                <i className="pi pi-tag"></i>
-                                            </span>
-                                            <InputText placeholder="Data name" name="dataName" value={this.state.dataName} onChange={this.handleChange}></InputText>
-                                        </div>
-                                    </div>
-                                    <div className="p-col-12">
-                                        <div className="p-inputgroup">
-                                            <span className="p-inputgroup-addon">
-                                                <i className="pi pi-user"></i>
-                                            </span>
-                                            <InputText placeholder="Data owner" name="dataOwner" value={this.state.dataOwner} onChange={this.handleChange}></InputText>
-                                        </div>
-                                    </div>
-                                    <div className="p-col-12">
-                                        <Calendar dateFormat="dd/mm/yy" value={this.state.date} onChange={(e) => this.setState({ date: e.value })} showIcon={true}></Calendar>
-                                    </div>
-                                    <div className="p-col-12">
-                                        <div className="p-inputgroup">
-                                            <span className="p-inputgroup-addon">
-                                                <i className="pi pi-globe"></i>
-                                            </span>
-                                            <InputText placeholder="EPSG" name="crs" value={this.state.crs} onChange={this.handleChange}></InputText>
-                                        </div>
-                                    </div>
-                                    
-                                    <div className="p-col-12">
-                                        <Button label="Upload" type="submit" />
-                                    </div>
-                                </div>
-                            </form>
-                        </Panel>
+                        <AddDataForm fetchUserDataSets={this.fetchUserDataSets} loadingService={this.props.loadingService} messageService={this.props.messageService}/>
                     </div>
                     <div className="p-col-12 p-lg-9 bathymetry-app-padding">
-                        <ContextMenu model={this.state.contextMenu} ref={el => this.cm = el} onHide={() => this.setState({selectedData: null})}/>
+                        <ContextMenu model={this.state.contextMenu} ref={el => this.cm = el} onHide={() => this.setState({ selectedData: null })} />
                         <DataTable header="My Datasets" value={this.state.data} responsive={true} contextMenuSelection={this.state.selectedData}
-                        onContextMenuSelectionChange={e => this.setState({selectedData: e.value})} onContextMenu={e => this.cm.show(e.originalEvent)}>
+                            onContextMenuSelectionChange={e => this.setState({ selectedData: e.value })} onContextMenu={e => this.cm.show(e.originalEvent)}>
                             <Column field="id" header="Id" />
                             <Column field="name" header="Name" />
                             <Column field="measurementDate" header="Date" />
