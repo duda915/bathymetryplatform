23a8cc181bc272f48a1034f00ab3d100d74d6da0
MDud <duda915@gmail.com>
2019-02-10 22:00:19 +0100

    APP refresh token axios interceptor

--- app/bathymetry-app/src/services/UserService.js
+++ app/bathymetry-app/src/services/UserService.js
@@ -11,8 +11,49 @@ export default class UserService {
         this.tokenEndpoint = this.backend + "oauth/token";
         this.userEndpoint = this.backend + "api/user";
         this.cookie = new Cookies();
-        
+
+        axios.interceptors.response.use(null, error => {
+            if (error.config && error.response && error.response.status === 401) {
+
+                const refreshTokenConfig = {
+                    headers: {
+                        'Authorization': this.serviceMeta.getBasicAuthorizationHeader()
+                    }
+                }
+
+
+                if(this.cookie.get('refresh_token') === 'signedout') {
+                    return Promise.reject(error);
+                }
+
+                if (this.cookie.get('refresh_token') === undefined) {
+                    this.cookie.set('refresh_token', 'signedout');
+                    window.location.reload();
+                }
+
+                const formData = new FormData();
+                formData.set('grant_type', 'refresh_token');
+                formData.set('refresh_token', this.cookie.get('refresh_token'));
+
+                axios.post(this.tokenEndpoint, formData, refreshTokenConfig)
+                    .then(response => {
+                        let accessTokenExpireDate = new Date();
+                        accessTokenExpireDate.setTime(accessTokenExpireDate.getTime() + 60 * 60 * 1000)
+                        this.cookie.set("access_token", response.data.access_token, { path: '/', expires: accessTokenExpireDate });
+                        error.config.headers['Authorization'] = 'Bearer ' + this.cookie.get('access_token');
+                        window.location.reload();
+                    }).catch(error => {
+                        console.log(error.response);
+                        this.cookie.remove("refresh_token");
+                        this.cookie.remove("access_token");
+                        window.location.reload();
+                    })
+            }
+
+            return Promise.reject(error);
+        });
     }
+
 
     async loginUser(username, password) {
         let formData = new FormData();
@@ -25,7 +66,7 @@ export default class UserService {
                 'Authorization': this.serviceMeta.getBasicAuthorizationHeader()
             }
         };
-        
+
         return axios.post(this.tokenEndpoint, formData, loginConfig)
             .then(response => this.saveTokens(response));
     }
@@ -36,7 +77,7 @@ export default class UserService {
 
     logoutUser() {
         this.cookie.remove("access_token", { path: '/' });
-        this.cookie.remove("refresh_token", { path: '/' });
+        this.cookie.set("refresh_token", 'signedout');
     }
 
     async getUser() {
@@ -49,7 +90,7 @@ export default class UserService {
         this.cookie.set("access_token", tokenResponse.data.access_token, { path: '/', expires: accessTokenExpireDate });
 
         let refreshTokenExpireDate = new Date();
-        refreshTokenExpireDate.setTime(refreshTokenExpireDate.getDate + 24 * 60 * 60 * 1000);
+        refreshTokenExpireDate.setTime(refreshTokenExpireDate.getTime() + 24 * 60 * 60 * 1000);
         this.cookie.set("refresh_token", tokenResponse.data.refresh_token, { path: '/', expires: refreshTokenExpireDate });
     }
 
