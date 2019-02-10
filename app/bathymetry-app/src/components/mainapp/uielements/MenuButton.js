7786406f3dacbc9d80ad4c09f61867429f698624
MDud <duda915@gmail.com>
2019-02-10 20:32:39 +0100

    APP login as guest disables settings and my data

--- app/bathymetry-app/src/components/mainapp/uielements/MenuButton.js
+++ app/bathymetry-app/src/components/mainapp/uielements/MenuButton.js
@@ -7,12 +7,22 @@ export default class MenuButton extends Component {
 
     render() {
         return (<div className="menu-button-container">
-            <NavLink activeClassName="menu-button-active" className="menu-button" exact to={this.props.to}>
-                <div className="menu-button-content-container">
-                    <div className="menu-button-icon"><i className={this.props.icon} /></div>
-                    <div className="menu-button-label">{this.props.label}</div>
-                </div>
-            </NavLink>
+            {this.props.disabled ?
+                <NavLink className="menu-button-disabled" exact to={this.props.to} onClick={ e => e.preventDefault()}>
+                    <div className="menu-button-content-container">
+                        <div className="menu-button-icon"><i className={this.props.icon} /></div>
+                        <div className="menu-button-label">{this.props.label}</div>
+                    </div>
+                </NavLink>
+                :
+                <NavLink activeClassName="menu-button-active" className="menu-button" exact to={this.props.to}>
+                    <div className="menu-button-content-container">
+                        <div className="menu-button-icon"><i className={this.props.icon} /></div>
+                        <div className="menu-button-label">{this.props.label}</div>
+                    </div>
+                </NavLink>
+            }
+
         </div>);
     }
 
--- app/bathymetry-app/src/components/mainapp/uielements/MenuPanel.js
+++ app/bathymetry-app/src/components/mainapp/uielements/MenuPanel.js
@@ -39,43 +39,12 @@ class MenuPanel extends Component {
     checkUser() {
         this.userService.getUser()
             .then(response => {
-                // if(response.data.userRoles[0].role.roleName === "GUEST") {
-                //     this.setState({
-                //         isGuest: true
-                //     });
-                //     window.location.hash="/";
-                // } else {
-                //     this.setState({
-                //         isGuest: false
-                //     })
-                // }
-                this.generateMenu();
+                if(response.data.username === 'guest') {
+                    this.setState({
+                        guest: true
+                    })
+                }
             });
-    }
-
-    generateMenu() {
-        this.setState({
-            menuItems: [
-                {
-                    label: '',
-                    items: [{ label: 'Map', icon: 'pi pi-map-marker', command: () => { window.location.hash = "/"; } },
-                    { label: 'Select Data', icon: 'pi pi-search', command: () => { window.location.hash = "/select"; } },
-                    { label: 'My Data', disabled: this.state.isGuest, icon: 'pi pi-cloud-upload', command: () => { window.location.hash = "/mydata"; } },
-                    { label: 'Settings', icon: 'pi pi-cog', command: () => { this.props.signOut() } },
-                    { label: 'Logout', icon: 'pi pi-sign-out', command: () => { this.props.signOut() } }
-                    ],
-                },
-                // {
-                //     label: 'Data',
-                //     items: [{ label: 'Select Data', icon: 'pi pi-search', command: () => { window.location.hash = "/select"; } },
-                //     { label: 'My Data', disabled: this.state.isGuest, icon: 'pi pi-cloud-upload', command: () => { window.location.hash = "/mydata"; } }]
-                // },
-                // {
-                //     label: 'User',
-                //     items: [{ label: 'Logout', icon: 'pi pi-sign-out', command: () => { this.props.logoutFun() } }],
-                // }
-            ],
-        });
     }
 
     render() {
@@ -96,7 +65,7 @@ class MenuPanel extends Component {
                                 <div className="menu-top"><div className="menu-header">Menu</div></div>
                                 <MenuButton to="/" label="Map" icon="pi pi-map-marker" />
                                 <MenuButton to="/select" label="Select Data" icon="pi pi-search" />
-                                <MenuButton to="/mydata" label="My Data" icon="pi pi-cloud-upload" />
+                                <MenuButton to="/mydata" disabled={this.state.guest} label="My Data" icon="pi pi-cloud-upload" />
                             </div>
                         </HashRouter>
                     </div>
@@ -105,7 +74,7 @@ class MenuPanel extends Component {
                     <div className="p-col-12 p-col-align-center">
                         <HashRouter>
                             <div className="router-container">
-                                <MenuButton to="/settings" label="Settings" icon="pi pi-cog" />
+                                <MenuButton to="/settings" disabled={this.state.guest} label="Settings" icon="pi pi-cog" />
                                 <MenuButtonOnClick to="/" label="Logout" icon="pi pi-sign-out" onClick={this.props.signOut} />
                             </div>
 
