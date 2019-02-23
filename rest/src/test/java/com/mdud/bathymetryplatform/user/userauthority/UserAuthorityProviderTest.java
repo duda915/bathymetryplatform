3699c07371caeafa5de332a4727f47b8c1fa0a23
MDud <duda915@gmail.com>
2019-02-23 13:06:02 +0100

    REST UserAuthorityProviderTest

---
+++ rest/src/test/java/com/mdud/bathymetryplatform/user/userauthority/UserAuthorityProviderTest.java
@@ -0,0 +1,32 @@
+package com.mdud.bathymetryplatform.user.userauthority;
+
+import com.mdud.bathymetryplatform.user.authority.Authorities;
+import com.mdud.bathymetryplatform.user.authority.Authority;
+import com.mdud.bathymetryplatform.user.authority.AuthorityRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserAuthorityProviderTest {
+
+    @InjectMocks
+    private UserAuthorityProvider userAuthorityProvider;
+
+    @Mock
+    public AuthorityRepository authorityRepository;
+
+    @Test
+    public void getUserAuthority_ShouldReturnAuthority() {
+        when(authorityRepository.findAuthorityByAuthorityName(Authorities.READ)).thenReturn(new Authority(Authorities.READ));
+        UserAuthority authority = userAuthorityProvider.getUserAuthority(Authorities.READ);
+
+        assertEquals(Authorities.READ, authority.getAuthority().getAuthorityName());
+        verify(authorityRepository, times(1)).findAuthorityByAuthorityName(Authorities.READ);
+    }
+}
