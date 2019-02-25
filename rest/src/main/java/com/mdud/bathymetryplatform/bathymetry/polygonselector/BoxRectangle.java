9f56e90f187006f21cb6da6a17e125588a0875fa
MDud <duda915@gmail.com>
2019-02-25 10:56:21 +0100

    REST bathymetrydatasetcontroller unit tests

--- rest/src/main/java/com/mdud/bathymetryplatform/bathymetry/polygonselector/BoxRectangle.java
+++ rest/src/main/java/com/mdud/bathymetryplatform/bathymetry/polygonselector/BoxRectangle.java
@@ -4,7 +4,12 @@ import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.geom.PrecisionModel;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
+@EqualsAndHashCode
+@NoArgsConstructor
 public class BoxRectangle implements GeometryAdapter<BoxRectangle>{
     private Coordinate upperLeftVertex;
     private Coordinate lowerRightVertex;
--- rest/src/test/java/com/mdud/bathymetryplatform/bathymetry/BathymetryDataSetControllerTest.java
+++ rest/src/test/java/com/mdud/bathymetryplatform/bathymetry/BathymetryDataSetControllerTest.java
@@ -1,5 +1,7 @@
 package com.mdud.bathymetryplatform.bathymetry;
 
+import com.mdud.bathymetryplatform.bathymetry.point.BathymetryPoint;
+import com.mdud.bathymetryplatform.bathymetry.polygonselector.BoxRectangle;
 import com.mdud.bathymetryplatform.exception.MethodArgumentNotValidExceptionHandler;
 import com.mdud.bathymetryplatform.gdal.GDALService;
 import com.mdud.bathymetryplatform.geoserver.GeoServerService;
@@ -9,7 +11,7 @@ import com.mdud.bathymetryplatform.user.ApplicationUserService;
 import com.mdud.bathymetryplatform.user.authority.Authorities;
 import com.mdud.bathymetryplatform.utility.JSONUtil;
 import com.mdud.bathymetryplatform.utility.SQLDateBuilder;
-import org.hamcrest.CoreMatchers;
+import com.vividsolutions.jts.geom.Coordinate;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,22 +19,22 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.ContentResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.allOf;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 public class BathymetryDataSetControllerTest {
@@ -160,5 +162,110 @@ public class BathymetryDataSetControllerTest {
 
     }
 
+    @Test
+    public void removeDataSet() throws Exception {
+        Principal principal = () -> "user";
 
+        mockMvc.perform(delete(endpoint)
+                .param("id", "1")
+                .principal(principal))
+                .andExpect(status().isOk());
+
+        verify(geoServerService, times(1)).deleteCoverageStore(1L);
+        verify(bathymetryDataSetService, times(1)).removeDataSet(principal.getName(), 1L);
+    }
+
+
+    @Test
+    public void downloadDataSetsByIds() throws Exception {
+        Principal principal = () -> "user";
+        ApplicationUserDTO applicationUserDTO = new ApplicationUserDTO("", "", "");
+        ApplicationUser applicationUser = new ApplicationUser(applicationUserDTO);
+
+        BathymetryDataSetDTO bathymetryDataSetDTO = new BathymetryDataSetDTO(applicationUser, 32634, "name", SQLDateBuilder.now(),
+                "data");
+        BathymetryDataSet bathymetryDataSet = new BathymetryDataSet(bathymetryDataSetDTO, new ArrayList<>());
+        when(bathymetryDataSetService.getDataSet(1L)).thenReturn(bathymetryDataSet);
+        when(bathymetryDataSetService.getDataSet(2L)).thenReturn(bathymetryDataSet);
+
+        mockMvc.perform(get(endpoint + "/download")
+                .param("id", "1")
+                .param("id", "2")
+                .principal(principal))
+                .andExpect(status().isOk());
+
+        verify(bathymetryDataSetService, times(1)).getDataSet(1L);
+        verify(bathymetryDataSetService, times(1)).getDataSet(2L);
+    }
+
+    @Test
+    public void downloadDataSetsBySelection() throws Exception {
+        Principal principal = () -> "user";
+
+        BoxRectangle boxRectangle = new BoxRectangle(new Coordinate(12, 12), new Coordinate(13, 13));
+        String json = JSONUtil.convertObjectToJsonString(boxRectangle);
+
+        when(bathymetryDataSetService.getAllBathymetryPointsWithinGeometry(1L, boxRectangle)).thenReturn(new ArrayList<>());
+        when(bathymetryDataSetService.getAllBathymetryPointsWithinGeometry(2L, boxRectangle)).thenReturn(new ArrayList<>());
+
+        mockMvc.perform(post(endpoint + "/download/selection")
+                .param("id", "1")
+                .param("id", "2")
+                .principal(principal)
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(json))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+
+        verify(bathymetryDataSetService, times(1)).getAllBathymetryPointsWithinGeometry(1L, boxRectangle);
+        verify(bathymetryDataSetService, times(1)).getAllBathymetryPointsWithinGeometry(2L, boxRectangle);
+    }
+
+    @Test
+    public void countDataSetsBySelection() throws Exception {
+        Principal principal = () -> "user";
+
+        BoxRectangle boxRectangle = new BoxRectangle(new Coordinate(12, 12), new Coordinate(13, 13));
+        String json = JSONUtil.convertObjectToJsonString(boxRectangle);
+
+        when(bathymetryDataSetService.getAllBathymetryPointsWithinGeometry(1L, boxRectangle)).thenReturn(new ArrayList<>());
+        when(bathymetryDataSetService.getAllBathymetryPointsWithinGeometry(2L, boxRectangle)).thenReturn(new ArrayList<>());
+
+        mockMvc.perform(post(endpoint + "/download/selection/count")
+                .param("id", "1")
+                .param("id", "2")
+                .principal(principal)
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(json))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.response").value(0));
+
+
+        verify(bathymetryDataSetService, times(1)).getAllBathymetryPointsWithinGeometry(1L, boxRectangle);
+        verify(bathymetryDataSetService, times(1)).getAllBathymetryPointsWithinGeometry(2L, boxRectangle);
+    }
+
+    @Test
+    public void getDataSetCenter() throws Exception {
+        when(geoServerService.getCoverageStoreCenter(1L)).thenReturn(new Coordinate());
+
+        mockMvc.perform(get(endpoint + "/center")
+                .param("id", "1"))
+                .andExpect(status().isOk());
+
+        verify(geoServerService, times(1)).getCoverageStoreCenter(1L);
+    }
+
+    @Test
+    public void getDataSetBoundingBox() throws Exception {
+        when(geoServerService.getCoverageStoreBoundingBox(1L)).thenReturn(new BoxRectangle());
+
+        mockMvc.perform(get(endpoint + "/box")
+                .param("id", "1"))
+                .andExpect(status().isOk());
+
+        verify(geoServerService, times(1)).getCoverageStoreBoundingBox(1L);
+    }
 }
