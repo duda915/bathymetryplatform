30bf9e59970cc9004240bf16e0a8c362a22de351
MDud <duda915@gmail.com>
2019-03-18 23:45:30 +0100

    unified docker composes

--- docker/first/postgiscontainer/init.sh
+++
@@ -1,9 +0,0 @@
-export PGUSER=bathymetry
-export PGPASSWORD=bathymetry
-psql <<- EOSQL
-    CREATE DATABASE xxx;
-EOSQL
-
-# sudo postgres bathymetry --single <<- EOSQL
-#     CREATE DATABASE xxx;
-# EOSQL
