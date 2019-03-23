259e13a53da5c3012a456c5ee1c6c5e54a89945e
MDud <duda915@gmail.com>
2019-03-23 16:39:01 +0100

    APP new map actions and reducers

--- app/bathymetry-app/src/components/app/map/MapActions.js
+++ app/bathymetry-app/src/components/app/map/MapActions.js
@@ -1,5 +1,11 @@
 export const FETCH_FEATURE_INFO = "FETCH_FEATURE_INFO";
 export const MAP_DRAG_BOX = "MAP_DRAG_BOX";
+export const ZOOM_TO_LAYER = "ZOOM_TO_LAYER";
+export const ZOOM_TO_FIT = "ZOOM_TO_FIT";
+export const TOGGLE_LAYER = "TOGGLE_LAYER";
+export const TOGGLE_STYLE = "TOGGLE_STYLE";
+export const ADD_LAYER = "ADD_LAYER";
+export const REMOVE_LAYERS = "REMOVE_LAYERS";
 
 export function fetchFeatureInfo(url) {
   return {
@@ -16,5 +22,92 @@ export function registerDragBox(box) {
     payload: {
       box
     }
+  };
+}
+
+export function deleteDragBox() {
+  return {
+    type: MAP_DRAG_BOX,
+    payload: {
+      box: null
+    }
+  };
+}
+
+export function requestZoomToLayer(layerId) {
+  return {
+    type: ZOOM_TO_LAYER,
+    payload: {
+      layerId
+    }
+  };
+}
+
+export function handledZoomToLayer() {
+  return {
+    type: ZOOM_TO_LAYER,
+    payload: {
+      layerId: null
+    }
+  };
+}
+
+export function requestZoomToFit() {
+  return {
+    type: ZOOM_TO_FIT,
+    payload: {
+      requestZoom: true
+    }
+  };
+}
+
+export function handledZoomToFit() {
+  return {
+    type: ZOOM_TO_FIT,
+    payload: {
+      requestZoom: false
+    }
+  };
+}
+
+export function toggleLayer(layerId) {
+  return {
+    type: TOGGLE_LAYER,
+    payload: {
+      layerId
+    }
+  };
+}
+
+export function requestToggleStyle() {
+  return {
+    type: TOGGLE_STYLE,
+    payload: {
+      requestToggleStyle: true
+    }
+  };
+}
+
+export function handledToggleStyle() {
+  return {
+    type: TOGGLE_STYLE,
+    payload: {
+      requestToggleStyle: false
+    }
+  };
+}
+
+export function addLayer(layer) {
+  return {
+    type: ADD_LAYER,
+    payload: {
+      layer
+    }
+  };
+}
+
+export function removeLayers() {
+  return {
+    type: REMOVE_LAYERS
   };
 }
--- app/bathymetry-app/src/components/app/map/MapReducer.js
+++ app/bathymetry-app/src/components/app/map/MapReducer.js
@@ -1,4 +1,13 @@
-import { FETCH_FEATURE_INFO, MAP_DRAG_BOX } from "./MapActions";
+import {
+  FETCH_FEATURE_INFO,
+  MAP_DRAG_BOX,
+  TOGGLE_LAYER,
+  TOGGLE_STYLE,
+  ZOOM_TO_FIT,
+  ZOOM_TO_LAYER,
+  ADD_LAYER,
+  REMOVE_LAYERS
+} from "./MapActions";
 import { combineReducers } from "redux";
 
 function featureInfoUrl(state = null, action) {
@@ -19,9 +28,57 @@ function dragBoxCoordinates(state = {}, action) {
   }
 }
 
+function style(state = false, action) {
+  switch (action.type) {
+    case TOGGLE_STYLE:
+      return action.payload.requestToggleStyle;
+    default:
+      return state;
+  }
+}
+
+function zoomToFit(state = false, action) {
+  switch (action.type) {
+    case ZOOM_TO_FIT:
+      return action.payload.requestZoom;
+    default:
+      return state;
+  }
+}
+
+function zoomToLayer(state = null, action) {
+  switch (action.type) {
+    case ZOOM_TO_LAYER:
+      return action.payload.layerId;
+    default:
+      return state;
+  }
+}
+
+function layers(state = [], action) {
+  switch (action.type) {
+    case ADD_LAYER:
+      return [...state, action.payload.layer];
+    case REMOVE_LAYERS:
+      return [];
+    case TOGGLE_LAYER:
+      return state.map(layer => {
+        if (layer.id === action.payload.layerId) {
+          layer.visible = !layer.visible;
+        }
+      });
+    default:
+      return state;
+  }
+}
+
 const map = combineReducers({
   featureInfoUrl,
-  dragBoxCoordinates
+  dragBoxCoordinates,
+  style,
+  zoomToFit,
+  zoomToLayer,
+  layers
 });
 
 export default map;
