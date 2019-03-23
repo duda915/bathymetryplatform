8b8209f6844f1f265ff718bcb5dcec04bcbab5f1
MDud <duda915@gmail.com>
2019-03-23 19:39:36 +0100

    APP changed app component to function and removed old code

--- app/bathymetry-app/src/App.js
+++ app/bathymetry-app/src/App.js
@@ -1,56 +1,20 @@
-import React, { Component } from "react";
-import Cookies from "universal-cookie";
-
-import "primereact/resources/primereact.min.css";
-import "./theme/PrimeReactTheme.scss";
 import "primeflex/primeflex.css";
 import "primeicons/primeicons.css";
+import "primereact/resources/primereact.min.css";
+import React from "react";
+import EntryPoint from "./components/EntryPoint";
+import LoadingSpinner from "./components/utility/loading/Spinner";
+import Message from "./components/utility/messaging/Message";
+import "./theme/PrimeReactTheme.scss";
 import "./theme/Theme.css";
 import "./theme/Utility.css";
 
-import LoadingSpinner from "./components/utility/loading/Spinner";
-import Message from "./components/utility/messaging/Message";
-import EntryPoint from "./components/EntryPoint";
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isLoggedIn: false
-    };
-
-    this.changeLoginState = this.changeLoginState.bind(this);
-    this.signIn = this.signIn.bind(this);
-    this.signOut = this.signOut.bind(this);
-  }
-
-  changeLoginState(loginState) {
-    this.setState({
-      isLoggedIn: loginState
-    });
-  }
-
-  signIn() {
-    this.changeLoginState(true);
-  }
-
-  signOut() {
-    this.changeLoginState(false);
-    const cookies = new Cookies();
-    cookies.remove("access_token");
-    cookies.remove("refresh_token");
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <LoadingSpinner />
-        <Message />
-        <EntryPoint />
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div>
+      <LoadingSpinner />
+      <Message />
+      <EntryPoint />
+    </div>
+  );
 }
-
-export default App;
