348cab80b79d0baf4e6f9ff62f12da0699cff7e4
MDud <duda915@gmail.com>
2019-03-24 21:48:49 +0100

    APP select data styling

--- app/bathymetry-app/src/components/app/dataselector/DataSelector.js
+++ app/bathymetry-app/src/components/app/dataselector/DataSelector.js
@@ -9,6 +9,7 @@ import { addLayers, removeLayers } from "../map/MapActions";
 import React, { Component } from "react";
 import API from "../../../services/API";
 import { handleRequest } from "../../utility/requesthandler";
+import "./DataSelector.scss";
 
 export class DataSelectorComponent extends Component {
   constructor(props) {
@@ -69,64 +70,62 @@ export class DataSelectorComponent extends Component {
   }
 
   render() {
-    var header = (
-      <div style={{ textAlign: "left" }}>
-        <div className="p-grid p-nogutter">
-          <div className="p-col-8">
-            <i className="pi pi-search" style={{ margin: "4px 4px 0 0" }} />
-            <span>Select Data</span>
-          </div>
-          <div className="p-col-4">
-            <i className="pi pi-search" style={{ margin: "4px 4px 0 0" }} />
-            <InputText
-              style={{ width: "45%" }}
-              type="search"
-              onInput={e => this.setState({ globalFilter: e.target.value })}
-              placeholder="Search"
-              size="2"
-            />
-          </div>
-        </div>
-      </div>
-    );
-
     return (
-      <div className="bathymetry-app-padding">
-        <div className="p-grid p-fluid ">
-          <div className="p-col-12">
-            <ContextMenu
-              model={this.state.contextMenu}
-              ref={el => (this.cm = el)}
-              onHide={() => this.setState({ selectedData: null })}
-            />
-            <DataTable
-              header={header}
-              globalFilter={this.state.globalFilter}
-              value={this.state.data}
-              responsive={true}
-              scrollable={true}
-              scrollHeight="75vh"
-              selectionMode="multiple"
-              selection={this.state.selection}
-              onSelectionChange={e => this.setState({ selection: e.value })}
-              metaKeySelection={false}
-              contextMenuSelection={this.state.selectedData}
-              onContextMenuSelectionChange={e =>
-                this.setState({ selectedData: e.value })
-              }
-              onContextMenu={e => this.cm.show(e.originalEvent)}
-            >
-              <Column field="id" header="Id" sortable={true} />
-              <Column field="name" header="Name" sortable={true} />
-              <Column field="measurementDate" header="Date" sortable={true} />
-              <Column field="dataOwner" header="Owner" sortable={true} />
-            </DataTable>
-          </div>
-          <div className="p-col-10" />
-          <div className="p-col-2">
-            <Button label="Show" onClick={this.showOnMap} />
-          </div>
+      <div className="p-grid p-nogutter p-justify-center data-selector-container">
+        <div className="p-col-12 p-md-1" />
+        <div className="p-col-12 p-md-10" style={{ padding: "10px" }}>
+          Select Data
         </div>
+        <div className="p-col-12 p-md-1" />
+
+        <div className="p-col-12 p-md-1" />
+        <div className="p-col-12 p-md-2" style={{ padding: "10px" }}>
+          <InputText
+            style={{ width: "100%" }}
+            type="search"
+            onInput={e => this.setState({ globalFilter: e.target.value })}
+            placeholder="Search"
+            size="2"
+          />
+        </div>
+        <div className="p-col-12 p-md-9" />
+
+        <div className="p-col-12 p-md-1" />
+        <div className="p-col-12 p-md-2" style={{ padding: "10px" }}>
+          <Button label="Show Selection" onClick={this.showOnMap} />
+        </div>
+        <div className="p-col-12 p-md-9" />
+
+        <div className="p-col-12 p-md-1" />
+        <div className="p-col-12 p-md-10 data-selector-table">
+          <ContextMenu
+            model={this.state.contextMenu}
+            ref={el => (this.cm = el)}
+            onHide={() => this.setState({ selectedData: null })}
+          />
+          <DataTable
+            globalFilter={this.state.globalFilter}
+            value={this.state.data}
+            paginator={true}
+            rows={20}
+            rowsPerPageOptions={[5,10,20]}
+            selectionMode="multiple"
+            selection={this.state.selection}
+            onSelectionChange={e => this.setState({ selection: e.value })}
+            metaKeySelection={false}
+            contextMenuSelection={this.state.selectedData}
+            onContextMenuSelectionChange={e =>
+              this.setState({ selectedData: e.value })
+            }
+            onContextMenu={e => this.cm.show(e.originalEvent)}
+          >
+            <Column field="id" header="Id" sortable={true} />
+            <Column field="name" header="Name" sortable={true} />
+            <Column field="measurementDate" header="Date" sortable={true} />
+            <Column field="dataOwner" header="Owner" sortable={true} />
+          </DataTable>
+        </div>
+        <div className="p-col-12 p-md-1" />
       </div>
     );
   }
